b0ba6fccd5fc389e788f71122e69843f1d83abf6
Tasakio Saken <31171075+Tasakio@users.noreply.github.com>
2018-01-07 14:29:19 +0000

    Updated to include Advanced Authentication
    Fluff Text

--- README.md
+++ README.md
@@ -1,19 +1,23 @@
 # Strife Bot
-is a Discord Bot designed for FFXIV Discord Servers to notify new users that join your server to use the command you have specified to change their Discord Server nickname to their FFXIV Character Name. Upon calling the command they are automatically assigned a Role.
+is a Discord Bot designed for FFXIV Discord Servers to notify new users that join your server to change their Discord Server nickname to their FFXIV Character Name.
 
 ## Setup
 Use your preferred method of obtaining this repository, open the folder in whichever Terminal application you prefer and run **npm install** to install the dependencies.
 
-You will find a file named **cpy_auth.json**, rename this to **auth.json** and edit the file to your own liking. I've made it so that everything can be changed and even altered for a completely different game if needed with support for the following:
+You will find a file named **cpy_auth.json**, rename this to **auth.json** and edit the file to your own liking.
 
-* **Bot Token** - This is your own Bot Token, required to log the bot in.
-* **Game** - Whatever Game you want the Bot to be displayed as playing.
-* **Command** - In the Example below this is set to **name** *(!name)*
-* **Channel** - The Channel the bot listens to for new users **MUST BE CHANNEL ID**
-* **Role** - The Bot can assign a Role you set **MUST BE THE ROLE NAME**
-* **Welcome Message** - What ever you want to say to the new user.
-* **Thank You Message** - What ever you want replied back to the user after setting their name.
+* **Bot Token** - You can Generate a Bot Token [HERE](https://discordapp.com/developers/applications/me)
+* **Channel** - This must be the ID of the Channel you wish the Bot to listen to for new users
+* **Role** - Please use the Name of the Role, not an ID
+* **Advanced Mode** - If set to False it requires `!name First Second` but if set to True then it generates an **MD5** Code and Direct Messages the User with instructions to Verify through Lodestone
 
-### Example
+##### Required Permissions
+* Manage Roles
+* Manage Nicknames
+* Send Messages
 
-![PreviewImage](https://i.imgur.com/RH844zc.png)
+## Built With
+* [Discord.js](https://www.npmjs.com/package/discord.js) - *discord.js is a powerful node.js module that allows you to interact with the Discord API very easily.*
+* [Request](https://www.npmjs.com/package/request) - *Request is designed to be the simplest way possible to make http calls.*
+* [Cheerio](https://www.npmjs.com/package/cheerio) - *Cheerio parses markup and provides an API for traversing/manipulating the resulting data structure.*
+* [MD5](https://www.npmjs.com/package/md5) - *a JavaScript function for hashing messages with MD5.*
--- app.js
+++ app.js
@@ -1,29 +1,75 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
+var request = require('request');
+var cheerio = require('cheerio');
+var md5 = require('md5');
 const auth = require('./auth.json');
 var prefix = "!";
 
 client.on("ready", () => {
-  console.log("Ready...");
-  client.user.setGame(auth.game);
+  console.log("Waiting...");
+  client.user.setGame("FINAL FANTASY XIV - A Realm Reborn");
 });
 
 client.on("guildMemberAdd", member => {
-  let channel = client.channels.get(auth.channel);
-  channel.send("<@"+member.user.id+"> " + auth.newMessage);
+  if(auth.advancedMode) {
+    let code = md5(member.user + auth.serverName.slice(1, 4));
+    member.user.send({embed: {
+    color: 3447003,
+    description: "Strife-Bot is currently set to Advanced Mode, this means you are required to verify that the Character is yours. Please follow the steps below to verify your account, if you need help please contact an Admin on " + auth.serverName + ".",
+    author: {
+      name: client.user.username,
+      icon_url: client.user.avatarURL
+    },fields: [{
+        name: "Your Verification Code",
+        value: "Please edit your [Character Profile](https://eu.finalfantasyxiv.com/lodestone/my/setting/profile/) to the following Verification Code: `" + code + "`"
+      },
+      {
+          name: "Verifying your Account",
+          value: "Once you have confirmed the changes, please copy your Profile ID from the URL of your Profile Page *(e.g. lodestone/character/ProfileID/)* and then type `!verify CODE` on " + auth.serverName
+      }
+    ],
+  }
+  });
+  } else {
+    let channel = client.channels.get(auth.channel);
+    channel.send("<@"+member.user.id+"> please set your Discord Nickname to your FFXIV Character Name with `!name Firstname Secondname` to gain access to the Member Areas.").catch(console.error);
+  }
 });
 
 client.on("message", (message) => {
-  let command = message.content.toLowerCase().split(" ")[0];
-  command = command.slice(prefix.length);
-  if(command === auth.command) {
-    let arg = message.content.split(" ");
-    let name = arg[1] + " " + arg[2];
-    message.member.setNickname(name).catch(console.error);
-    let role = message.guild.roles.find("name", auth.role);
-    message.member.addRole(role).catch(console.error);
-    message.channel.send(message.author + " " + auth.replyMessage);
-  }
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+  if(auth.advancedMode) {
+    if(message.content.startsWith(prefix + "verify")) {
+      let arg = message.content.split(" ");
+      let profile = arg[1];
+      let code = md5(message.user + auth.serverName.slice(1, 4));
+
+      request('https://eu.finalfantasyxiv.com/lodestone/character/' + profile + '/', function (error, response, html) {
+        if (!error && response.statusCode == 200) {
+          var $ = cheerio.load(html);
+          var characterName = $('.frame__chara__name')[0].children[0].data;
+          var bioCode = $('.character__selfintroduction')[0].children[0].data.replace(/\s+/g, '');
+          if(bioCode.indexOf(code) !== -1) {
+            message.member.setNickname(characterName).catch(console.error);
+            let role = message.guild.roles.find("name", auth.role);
+            message.member.addRole(role).catch(console.error);
+          } else {
+            message.channel.send(message.author + " I'm sorry but I could not verify your profile, please contact an Admin.");
+          }
+        }
+      });
+    }
+  } else {
+    if(message.content.startsWith(prefix + "name")) {
+      let arg = message.content.split(" ");
+      let name = arg[1] + " " + arg[2];
+      message.member.setNickname(name).catch(console.error);
+      let role = message.guild.roles.find("name", auth.role);
+      message.member.addRole(role).catch(console.error);
+    }
+}
 });
 
 client.login(auth.token);
